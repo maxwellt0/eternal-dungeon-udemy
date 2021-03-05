3c7b50adebfe01de29ebbaf183a1780476bbc2de
Maxwellt <maksimvinnik@gmail.com>
2021-03-05 18:16:29 +0200

    Make balls move

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -1,16 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using PathCreation;
 using UnityEngine;
 
 public class Board : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public BallSlot ballSlotPrefab;
+
+    private PathCreator pathCreator;
+
     void Start()
     {
+        pathCreator = FindObjectOfType<PathCreator>();
         
+        InitBallSlots();
     }
 
-    // Update is called once per frame
+    private void InitBallSlots()
+    {
+        float pathLength = pathCreator.path.length;
+        int slotsCount = (int) pathLength;
+        float step = pathLength / slotsCount;
+
+        for (int i = 0; i < slotsCount; i++)
+        {
+            float distanceTraveled = i * step;
+            Vector3 slotPos = pathCreator.path.GetPointAtDistance(distanceTraveled);
+            BallSlot ballSlot = Instantiate(ballSlotPrefab, slotPos, Quaternion.identity);
+            ballSlot.distanceTraveled = distanceTraveled;
+        }
+    }
+    
     void Update()
     {
         
