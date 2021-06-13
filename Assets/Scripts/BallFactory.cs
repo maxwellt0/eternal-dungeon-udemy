3b505e63274f2ec19a98ae34714c271ee11abf9f
Maxwellt <maksimvinnik@gmail.com>
2021-06-13 18:30:32 +0300

    Shoot balls

---
+++ Assets/Scripts/BallFactory.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallFactory : MonoBehaviour
+{
+    public Ball ballPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public Ball CreateBallAt(Vector3 point)
+    {
+        Ball ball = Instantiate(ballPrefab, point, Quaternion.identity);
+
+        return ball;
+    }
+}
